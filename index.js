8b8f4cf74ad045927f51b1a32e3168285262e478
ellijs <ellijs@hotmail.com>
2021-12-12 03:45:58 -0500

    mergesort lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+  function findMinAndRemoveSorted (array){
+    if(array.length < 2){
+      return array;
+    } else {
+      merge(findMinAndRemoveSorted (firstHalfArray), findMinAndRemoveSorted (secondHalfArray))
+    }
+  }
